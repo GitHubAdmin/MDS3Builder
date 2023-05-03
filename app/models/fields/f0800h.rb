a4bb5340bdd8d8019703e012a023940e77ef5433
Murtaza Khan <murtaza.khan@healthstream.com>
2023-05-02 19:26:38 -0500

    Changing F0800 fields to use checkbox

--- app/models/fields/f0800h.rb
+++ app/models/fields/f0800h.rb
@@ -3,17 +3,16 @@ class F0800h
 
   def initialize
     @name = "Staff Assessment of Daily and Activity Preferences: Resident prefers staying up past 8:00 p.m. (F0800h)"
-    @field_type = DROPDOWN
+    @field_type = CHECKBOX
     @node = "F0800H" 
 
     @options = []
-    @options << FieldOption.new("^", "NA")
-    @options << FieldOption.new("0", "No")
-    @options << FieldOption.new("1", "Yes")
+    @options << FieldOption.new("0")
+    @options << FieldOption.new("1", "True")
   end
 
   def set_values_for_type(klass)
-    return "^"
+    return "0"
   end
 
 end
--- app/models/fields/f0800k.rb
+++ app/models/fields/f0800k.rb
@@ -2,18 +2,17 @@ class F0800k
   attr_reader :options, :name, :field_type, :node
 
   def initialize
-    @name = "Staff Assessment of Daily and Activity Preferences: Resident prefers reading books, newspapers, or magazines (F0800K)"
-    @field_type = DROPDOWN
+    @name = "Staff Assessment of Daily and Activity Preferences: Resident prefers place to lock personal belongings (F0800K)"
+    @field_type = CHECKBOX
     @node = "F0800K" 
 
     @options = []
-    @options << FieldOption.new("^", "NA")
-    @options << FieldOption.new("0", "No")
-    @options << FieldOption.new("1", "Yes")
+    @options << FieldOption.new("0")
+    @options << FieldOption.new("1", "True")
   end
 
   def set_values_for_type(klass)
-    return "^"
+    return "0"
   end
 
 end
