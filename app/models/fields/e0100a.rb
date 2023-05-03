3c6d039597a71d97e7d595c73e975edfe233415c
Murtaza Khan <murtaza.khan@healthstream.com>
2023-05-02 19:18:30 -0500

    e0100 checkbox

--- app/models/fields/e0100a.rb
+++ app/models/fields/e0100a.rb
@@ -4,13 +4,12 @@ class E0100a
   def initialize
     @title = "Section E: Behavior"
     @name = "Potential Indicators of Psychosis: Hallucinations (E0100a)"
-    @field_type = DROPDOWN
+    @field_type = CHECKBOX
     @node = "E0100A"
 
     @options = []
-    @options << FieldOption.new("^", "NA")
-    @options << FieldOption.new("0", "No")
-    @options << FieldOption.new("1", "Yes")
+    @options << FieldOption.new("0")
+    @options << FieldOption.new("1", "True")
   end
 
   def set_values_for_type(klass)
